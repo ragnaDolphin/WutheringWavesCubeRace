cb31f44b40fb1ef55021e1fffafc37fb87249190
blackmoonhzh <ragnaDolphin@outlook.com>
2025-05-15 22:37:08 +0800

    Update sim.py
    增加晋级率计算

--- sim.py
+++ sim.py
@@ -1,7 +1,8 @@
 import cube_sim
 
+total_match_num = 1000
 result = {}
-for i in range(0,1000):
+for i in range(0,total_match_num):
     match_result = cube_sim.main()
     j = 1
     for name in match_result:
@@ -17,3 +18,5 @@ for key in result:
     rank = 1
     for rank in range(1,7):
         print("第%d名：%d次"%(rank,result[key][rank]))
+    win_rate = (result[key][1] + result[key][2] + result[key][3] + result[key][4]) / total_match_num * 100
+    print('晋级率：%.2f%%'%win_rate)
